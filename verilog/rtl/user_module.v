f87258aeeacd59074e23eb8ef9697deec017807e
Takuya Sasatani <33111879+t-sasatani@users.noreply.github.com>
2022-12-29 21:43:14 +0900

    modified user_module.v format

--- verilog/rtl/user_module.v
+++ verilog/rtl/user_module.v
@@ -1,18 +1,18 @@
 module user_module(
         input   wire    clk,
         input   wire    [7:0]   io_in,
-        output  wire    [7:0]   io_out
+        output  wire    [7:0]   out
     );
     
     reg [3:0]   clock_counter_a     =   4'b0000;
     reg [3:0]   clock_counter_b     =   4'b0000;
+    reg         clock_a              =   0;
+    reg         clock_b              =   0;
+
     wire [2:0]   clock_div_factor_a;
     wire [2:0]   clock_div_factor_b;
     wire        clock_select;
     wire        enable;
-    
-    reg         clock_a              =   0;
-    reg         clock_b              =   0;
     wire        clock_syn;
     
     assign  clock_select = io_in[0];
@@ -20,12 +20,7 @@ module user_module(
     assign  clock_syn = (enable)? ((clock_select) ? clock_a:clock_b):0;
     assign  clock_div_factor_a = io_in[4:2];
     assign  clock_div_factor_b = io_in[7:5];
-    assign io_out[0] = clock_syn;
-    assign io_out[1] = clock_a;
-    assign io_out[2] = clock_b;
-    assign io_out[3] = clk;
-    assign io_out[7:4] = 4'b0000;
-    
+
     always @ (posedge clk) begin
         clock_counter_a <= clock_counter_a + 1;
         clock_counter_b <= clock_counter_b + 1;
@@ -40,5 +35,11 @@ module user_module(
             clock_counter_b <= 4'b0000;
         end
     end
+    
+    assign out[0] = clock_syn;
+    assign out[1] = clock_a;
+    assign out[2] = clock_b;
+    assign out[3] = clk;
+    assign out[7:4] = 4'b0000;
     
 endmodule
